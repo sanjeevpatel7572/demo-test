36e3e564d78e6c0fff3c465d3b87c950326aa074
sanjeev madan singh <sanjeev.madan-singh@metlife.com>
2020-05-22 19:28:27 +0530

    initial commit

---
+++ src/main/java/com/example/demotest/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demotest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/test")
+	Object test() {
+		return "Hello, test example";
+	}
+
+}
